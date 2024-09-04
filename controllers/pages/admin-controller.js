279e1c9fb724a4551967fc18cac170dd942c0d34
Kriya <kriya101101@gmail.com>
2024-09-04 16:35:17 +0800

    feat: add api error handler & delete restaurant api

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -82,13 +82,11 @@ const adminController = {
       .catch(err => next(err))
   },
   deleteRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
-        if (!restaurant) throw new Error('Restaurant did not exist!')
-        return restaurant.destroy()
-      })
-      .then(() => res.redirect('/admin/restaurants'))
-      .catch(err => next(err))
+    admintServices.deleteRestaurant(req, (err, data) => {
+      if (err) return next(err)
+      req.session.deletedData = data
+      return res.redirect('/admin/restaurants')
+    })
   },
   getUsers: (req, res) => {
     return User.findAll({ raw: true })
--- services/admin-services.js
+++ services/admin-services.js
@@ -9,6 +9,15 @@ const adminController = {
     })
       .then(restaurants => callback(null, { restaurants }))
       .catch(err => callback(err))
+  },
+  deleteRestaurant: (req, cb) => {
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant did not exist!')
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
+      .catch(err => cb(err))
   }
 }
 
